ac19aacd76c12664e10acf15ad76f711ff942682
Nina Buchina <m.e.tigra@gmail.com>
2015-03-31 11:32:24 +0200

    Start building model for a program

--- translator/models.py
+++ translator/models.py
@@ -1,3 +1,11 @@
+from django.contrib.auth.models import User
 from django.db import models
 
 # Create your models here.
+from django.db.models import Model
+
+
+class RobotProgram(models.Model):
+    text_description = models.TextField
+    pickled_formal_description = models.TextField
+    user = models.ForeignKey(User)
