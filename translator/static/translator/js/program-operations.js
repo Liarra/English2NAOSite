a789e08f857bf44509d4804353b0f5df1b78e759
Nina Buchina <m.e.tigra@gmail.com>
2015-05-26 16:25:37 +0200

    Fronted: now info of components is loaded in separate divs, addition and change are implemented

--- translator/static/translator/js/program-operations.js
+++ translator/static/translator/js/program-operations.js
@@ -25,9 +25,17 @@ $(".program-box-clickable").click(
     function(){
     $(".active-box").removeClass("active-box");
     $(this).addClass("active-box");
-    substep_div=$(this).parent()
-    substepid=substep_div.children(".glyphicon-step").first().html()
-        load_component_params($(this).attr('about'), substepid)
+    substep_div=$(this).parent();
+    substepid=substep_div.children(".glyphicon-step").first().html();
+    load_component_params($(this).attr('about'), substepid,'s');
+    }
+);
+
+$(".added-action").click(
+    function(){
+    $(".active-box").removeClass("active-box");
+    $(this).addClass("active-box");
+    load_component_params($(this).attr('about'), 0,'');
     }
 );
 
@@ -38,6 +46,15 @@ $(".empty-program-box").click(
         load_actions_library();
     }
 );
+}
+
+function assignDoneButton(){
+    $(".btn-done").click(
+        function(){
+        collectAdditions();
+        collectModifications();
+        }
+    );
 }
 
 
@@ -81,11 +98,12 @@ function edit_substep(substep_div){
 
                     success: {
                       label: "Done!",
-                      className: "btn-success",
+                      className: "btn-done",
                     }
                  }
                 });
                 assign_substep_actions_icons();
+                assignDoneButton();
             },
 
             fail:function(data){
@@ -96,13 +114,22 @@ function edit_substep(substep_div){
 
 
 function load_actions_library(){
+
+     if($("#library").length ){
+        $(".action-params-div").hide();
+        $("#library").show();
+        return
+    }
+
     $.ajax({
             url: "/translator/editor-substep-actions/",
             type:"GET",
 
             success: function( data ) {
+                $(".action-params-div").hide();
+                new_div="<div class='action-params-div' id='library'>"+data+"</div>"
                 $("#substep_editor_library").show();
-                $("#substep_editor_library").html(data);
+                $("#substep_editor_library").append(new_div);
                 $(".new-action-icon").click(
                     function(){
                     addNewActionTemplate($(this));
@@ -117,7 +144,15 @@ function load_actions_library(){
 }
 
 
-function load_component_params(index, substepid){
+function load_component_params(index, substepid,letter){
+//If it's already there, just show it
+
+    if($("#"+letter+index).length ){
+        $(".action-params-div").hide();
+        $("#"+letter+index).show();
+        return
+    }
+
     $.ajax({
             url: "/translator/editor-substep-params/",
             type:"POST",
@@ -128,14 +163,56 @@ function load_component_params(index, substepid){
             },
 
             success: function( data ) {
+                $(".action-params-div").hide();
+                new_div="<div class='action-params-div' id='"+letter+index+"'>"+data+"</div>"
                 $("#substep_editor_library").show();
-                $("#substep_editor_library").html(data);
+                $("#substep_editor_library").append(new_div);
             },
 
             fail:function(data){
                 throw_exception;
             }
         });
+}
+
+function collectAdditions(){
+added_icons=$(".added-action");
+for (index = 0; index < added_icons.length; ++index) {
+    console.log(added_icons[index]);
+    var className=added_icons[index].attributes["command"].nodeValue;
+    var params_div_id=added_icons[index].attributes["about"].nodeValue;
+    var params_div=$("#"+params_div_id);
+    var param_textboxes=params_div.children("div").first().children("input");
+    var params = {};
+
+    for (jindex = 0; jindex < param_textboxes.length; ++jindex) {
+        name=param_textboxes[jindex].attributes["name"].nodeValue;
+        value=param_textboxes[jindex].value;
+        params[name]=value;
+    }
+
+    changelist.addAction(className,params);
+}
+}
+
+function collectModifications(){
+added_icons=$(".existing-action");
+for (index = 0; index < added_icons.length; ++index) {
+    console.log(added_icons[index]);
+    var params_div_id=added_icons[index].attributes["about"].nodeValue;
+    var params_div=$("#s"+params_div_id);
+    var param_textboxes=params_div.children("div").first().children("input");
+    var params = {};
+
+    for (jindex = 0; jindex < param_textboxes.length; ++jindex) {
+        name=param_textboxes[jindex].attributes["name"].nodeValue;
+        value=param_textboxes[jindex].value;
+        params[name]=value;
+    }
+
+    changelist.changeAction(index,params);
+}
+
 }
 
 function throw_exception(){
--- translator/static/translator/js/substep-operations.js
+++ translator/static/translator/js/substep-operations.js
@@ -8,35 +8,37 @@ var changelist={
     change_in_conditions:[],
 
     clear:function(){
-        actions_to_add=[];
-        actions_to_remove=[];
-        change_in_actions=[];
-        conditions_to_add=[];
-        conditions_to_remove=[];
-        change_in_conditions=[];
+        this.actions_to_add=[];
+        this.actions_to_remove=[];
+        this.change_in_actions=[];
+        this.conditions_to_add=[];
+        this.conditions_to_remove=[];
+        this.change_in_conditions=[];
     },
 
 
     addAction:function(new_class,new_params){
         var new_action={class: new_class, params:new_params};
-        actions_to_add.push(new_action);
+        this.actions_to_add.push(new_action);
     },
 
     addCondition:function(new_class,new_params){
         var new_condition={class: new_class, params:new_params};
-        conditions_to_add.push(new_condition);
-    }
+        this.conditions_to_add.push(new_condition);
+    },
 
-
-
+    changeAction:function(index,new_params){
+        this.change_in_actions[index]=new_params;
+    },
 
 }
 
-
+var i=0
 function addAllNewActions(){
 }
 
 function addNewActionTemplate(senderIcon){
+    i=i+1;
     new_class=senderIcon.attr('command');
     class_glyphicon="glyphicon-question";
 
@@ -47,7 +49,7 @@ function addNewActionTemplate(senderIcon){
     if (new_class=="wait_command")
     class_glyphicon="glyphicon-time";
 
-    $(" <span class='program-box glyphicon "+class_glyphicon+" program-box-clickable new-action added-action'></span>").insertBefore("#empty-action-box");
+    $(" <span class='program-box glyphicon "+class_glyphicon+" program-box-clickable new-action added-action' about='a"+i+"' command='"+new_class+"'></span>").insertBefore("#empty-action-box");
     $(".active-box").removeClass("active-box");
     $(".new-action").addClass("active-box");
     $(".new-action").removeClass("new-action");
@@ -60,7 +62,11 @@ function addNewActionTemplate(senderIcon){
             },
 
             success: function( data ) {
-                $("#substep_editor_library").html(data);
+                $(".action-params-div").hide();
+                new_div="<div class='action-params-div' id='a"+i+"'>"+data+"</div>"
+                $("#substep_editor_library").show();
+                $("#substep_editor_library").append(new_div);
+                assign_substep_actions_icons()
             },
 
             fail:function(data){
