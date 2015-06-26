1df67cb0058b0286a38a7c303e84bcf1c3168b27
Nina Buchina <m.e.tigra@gmail.com>
2015-06-26 12:45:39 +0200

    An initialiser for Grammar class and the method to add a new rule

--- translator/executables/nlp/grammar/grammar.py
+++ translator/executables/nlp/grammar/grammar.py
@@ -1,7 +1,23 @@
+from collections import OrderedDict
+
 __author__ = 'nina'
+
+
 class Grammar:
+    def __init__(self):
+        self.rules = OrderedDict()
+        self.word_names = OrderedDict()
+
     def appendRule(self, input, transformation=None):
-        pass
+        names_array = []
+        things_array = []
+        for name, thing in input:
+            names_array.append(name)
+            things_array.append(thing)
+
+        self.rules[tuple(things_array)] = transformation
+        self.word_names[tuple(things_array)] = tuple(names_array)
+
 
     def process(self, objects_stream):
         return []
--- translator/executables/nlp/grammar/tests/grammar_test.py
+++ translator/executables/nlp/grammar/tests/grammar_test.py
@@ -1,9 +1,10 @@
+from collections import OrderedDict
 from unittest import TestCase
-from translator.executables.nlp.components.execution import parallel
+from translator.executables.nlp.components.execution import Parallel
 from translator.executables.nlp.components.moves.demo_moves import dance
 from translator.executables.nlp.components.robot_commands import *
 from translator.executables.nlp.grammar.grammar import Grammar
-from translator.executables.nlp.substep import SubStep
+from translator.executables.nlp.states.state import State
 
 __author__ = 'nina'
 
@@ -13,7 +14,7 @@ class GrammarTests(TestCase):
         gr = Grammar()
 
         def rule(first, second, third, step_counter):
-            new_step = SubStep()
+            new_step = State()
             new_step.text_index_start = first.text_index_start
             new_step.tivipe_component_name = first.tivipe_component_name
             new_step.description = first.description + " " + third.description
@@ -24,10 +25,31 @@ class GrammarTests(TestCase):
             return new_step
 
         gr.appendRule(
-            input={"first": command, "second": parallel, "third": command},
+            input={"first": Command, "second": Parallel, "third": Command},
             transformation=rule
         )
 
-        new_sequence = gr.process([say_command.from_string("Say 'hi'"), parallel(), dance])
+        new_sequence = gr.process([say_command.from_string("Say 'hi'"), Parallel(), dance])
 
-        self.assertIsInstance(new_sequence[0], SubStep)
+        self.assertIsInstance(new_sequence[0], State)
+
+    def test_add_new_rule(self):
+        gr = Grammar()
+
+        def rule(first, second, third, step_counter):
+            new_step = State()
+            new_step.text_index_start = first.text_index_start
+            new_step.tivipe_component_name = first.tivipe_component_name
+            new_step.description = first.description + " " + third.description
+            new_step.ID = "%.2f" % step_counter
+            new_step.commands.append(first)
+            new_step.commands.append(third)
+
+            return new_step
+
+        gr.appendRule(
+            input=[("first", Command), ("second", Parallel), ("third", Command)],
+            transformation=rule
+        )
+
+        self.assertEquals(rule, gr.rules[Command, Parallel, Command])
